2a7e56b0dd5b2631a73638054fc8961165b35c0e
Sylvain UTARD <sylvain.utard@gmail.com>
2014-05-27 20:05:18 +0200

    Last meetup first

--- config/initializers/algolia.rb
+++ config/initializers/algolia.rb
@@ -11,5 +11,5 @@ RSVPS_INDEX.set_settings({
   attributesToIndex: ['name', 'bio', 'event.bio', 'city', 'unordered(event.name)', 'other_services.twitter.identifier', 'event.venue.name'],
   attributesForFaceting: ['topics.name', 'city', 'event.name', 'event.venue.name'],
   attributeForDistinct: 'uid',
-  customRanking: ['desc(membership_count)', 'asc(name)']
+  customRanking: ['desc(membership_count)', 'asc(name)', 'desc(event.time)']
 })
